a2831b9a7597f918f017b0c47fefc7413dc8fae9
naresh <saininaresh4435@gmail.com>
2022-01-25 12:21:26 +0530

    added files for mlflow

--- train.py
+++ train.py
@@ -62,5 +62,6 @@ if __name__ == "__main__":
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
+        mlflow.log_artifact("importance.png")
 
         mlflow.sklearn.log_model(lr, "model")
